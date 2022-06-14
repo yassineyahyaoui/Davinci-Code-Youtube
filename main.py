5ba24ae158612a7abd79ea78ad27628cbad9678f
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-06-14 11:19:00 +0100

    get at maximum 5 videos

--- main.py
+++ main.py
@@ -1,40 +1,32 @@
-# -*- coding: utf-8 -*-
 
-# Sample Python code for youtube.channels.list
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
-
-import os
-import json
 import csv
 import googleapiclient.discovery
 
 def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
     api_version = "v3"
     DEVELOPER_KEY = "AIzaSyCQ7pxDuHY2_bymJf0ZbqUFXIFQ36TLYdo"
 
-    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=DEVELOPER_KEY)
 
-    request = youtube.channels().list(
-        part="statistics",
-        id="UCc3yC3OMNYw-DEME69MVt_Q"
+    request = youtube.search().list(
+        part="snippet,id",
+        channelId="UCJvgF5uUL22U7i9tNlPvduA",
+        order="date",
+        maxResults=5
     )
     response = request.execute()
 
     print(response)
 
-    f = open("channels.csv", "a", newline="")
+    #f = open("channels.csv", "a", newline="")
 
-    row = (response["items"][0]["id"], response["items"][0]["statistics"]["videoCount"])
-    writer = csv.writer(f)
-    writer.writerow(row)
+    #row = (response["items"][0]["id"], response["items"][0]["statistics"]["videoCount"])
+    #writer = csv.writer(f)
+    #writer.writerow(row)
 
-    f.close()
+    #f.close()
 
 
 
@@ -42,5 +34,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
