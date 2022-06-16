b83ce817f98cf6ec21652bed61489809ab7e3d28
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-06-16 11:12:05 +0100

    check first if the video already exists

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import json
 import csv
 import googleapiclient.discovery
+
 
 def main():
 
     api_service_name = "youtube"
     api_version = "v3"
-    DEVELOPER_KEY = "AIzaSyCQ7pxDuHY2_bymJf0ZbqUFXIFQ36TLYdo"
+    developer_key = "AIzaSyCQ7pxDuHY2_bymJf0ZbqUFXIFQ36TLYdo"
 
-    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=DEVELOPER_KEY)
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=developer_key)
 
     request_videos = youtube.search().list(
         part="snippet,id",
@@ -17,8 +17,6 @@ def main():
         maxResults=50
     )
     response_videos = request_videos.execute()
-
-
 
     file_videos = open("videos.csv", "r", newline="")
     content = file_videos.read()
@@ -39,17 +37,27 @@ def main():
 
     for video in response_videos["items"]:
         if "videoId" in video["id"]:
-            file_videos = open("videos.csv", "a", newline="")
+            file_videos = open("videos.csv", "r", newline="")
 
-            channel_title = video["snippet"]["channelTitle"]
-            video_id = video["id"]["videoId"]
-            video_title = video["snippet"]["title"].encode("utf-8")
+            content = file_videos.read()
 
-            row = (channel_title, video_id, video_title)
+            if video["id"]["videoId"] not in content:
+                file_videos.close()
 
-            csv.writer(file_videos).writerow(row)
+                file_videos = open("videos.csv", "a", newline="")
 
-            file_videos.close()
+                channel_title = video["snippet"]["channelTitle"]
+                video_id = video["id"]["videoId"]
+                video_title = video["snippet"]["title"].encode("utf-8")
+
+                row = (channel_title, video_id, video_title)
+
+                csv.writer(file_videos).writerow(row)
+
+                file_videos.close()
+            else:
+                file_videos.close()
+
 
 if __name__ == "__main__":
     main()
