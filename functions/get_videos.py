c1fe6a9f22242b4dd6b8edb28c73caa580a2cc18
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-07-06 14:42:34 +0100

    fixing get videos and get 5 videos per channel

--- functions/get_videos.py
+++ functions/get_videos.py
@@ -9,14 +9,14 @@ api_version = "v3"
 developer_key = "AIzaSyCQ7pxDuHY2_bymJf0ZbqUFXIFQ36TLYdo"
 
 
-def get_videos(targeted_channel, targeted_channel_id):
+def get_videos(targeted_channel, video_id):
     youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=developer_key)
 
     request = youtube.search().list(
         part="snippet,id",
-        channelId=targeted_channel_id,
+        channelId=video_id,
         order="date",
-        maxResults=50
+        maxResults=5
     )
     response = request.execute()
 
@@ -44,7 +44,7 @@ def get_videos(targeted_channel, targeted_channel_id):
             if video["id"]["videoId"] not in content:
                 file_videos = open(os.path.join("data", targeted_channel, "videos.csv"), "a", newline="")
 
-                channel_title = video["snippet"]["channelTitle"]
+                channel_title = video["snippet"]["channelTitle"].encode("utf-8")
                 video_id = video["id"]["videoId"]
                 video_title = video["snippet"]["title"].encode("utf-8")
 
--- main.py
+++ main.py
@@ -7,17 +7,26 @@ import os
 
 def main():
     channels = []
-    file = open(os.path.join("data", "my_channels.csv"), "r", newline="")
-    content = csv.DictReader(file)
+    channels2 = []
+    file_my_channels = open(os.path.join("data", "my_channels.csv"), "r", newline="")
+    content = csv.DictReader(file_my_channels)
     for row in content:
         channels.append(row)
-    file.close()
+    file_my_channels.close()
 
     for channel in channels:
         if not os.path.exists(os.path.join("data", channel["Channel name"])):
             os.mkdir(os.path.join("data", channel["Channel name"]))
         get_subscriptions(channel["Channel name"])
-        get_videos(channel["Channel name"], channel["Channel id"])
+
+    for channel in channels:
+        file_videos = open(os.path.join("data", channel["Channel name"], "channels.csv"), "r", newline="")
+        content = csv.DictReader(file_videos)
+        for row in content:
+            print(row)
+            channels2.append(row)
+            get_videos(channel["Channel name"], row["Channel id"])
+        file_videos.close()
 
 
 if __name__ == "__main__":
