daffb02aabe98609d4f3ca022e9a06166d186855
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-07-23 16:12:14 +0100

    download_videos

--- functions/download_videos.py
+++ functions/download_videos.py
@@ -5,9 +5,11 @@ from pytube import YouTube
 
 def download_videos(targeted_channel, video_id):
     yt = YouTube("https://www.youtube.com/" + video_id)
-    yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download("C:/Users/DELL/PycharmProjects/Davinci-Code-Youtube/data/" + targeted_channel)
-    print("completed")
+    yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download("C:/Users/DELL/PycharmProjects/Davinci-Code-Youtube/data/" + targeted_channel + "/" + video_id)
+
     check_downloaded_videos_file(targeted_channel)
+
+    update_downloaded_videos_file(targeted_channel, video_id)
 
 
 def check_downloaded_videos_file(targeted_channel):
@@ -26,3 +28,21 @@ def check_downloaded_videos_file(targeted_channel):
         file_downloaded_videos.close()
 
 
+def update_downloaded_videos_file(targeted_channel, video_id):
+    videos_list = []
+    file_videos = open(os.path.join("data", targeted_channel, "videos.csv"), "r", newline="")
+    content = csv.DictReader(file_videos)
+    for video in content:
+        videos_list.append(video)
+    file_videos.close()
+
+    for video in videos_list:
+        if video["Video id"] == video_id:
+            file_downloaded_videos = open(os.path.join("data", targeted_channel, "downloaded_videos.csv"), "a",
+                                          newline="")
+            row = (video["Channel name"], video["Video id"], video["Video title"], video["Video description"],
+                   video["Video rating"], video["Video view count"], video["Video like count"],
+                   video["Video comment count"], video["Video license"], video["Video duration"],
+                   video["Video publish time"])
+            csv.writer(file_downloaded_videos).writerow(row)
+            file_downloaded_videos.close()
