ca6329b7ba5a849c5992aaaf7c42a1a00ca13339
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-07-23 21:50:25 +0100

    update_videos_file

--- functions/download_videos.py
+++ functions/download_videos.py
@@ -8,8 +8,8 @@ def download_videos(targeted_channel, video_id):
     yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download("C:/Users/DELL/PycharmProjects/Davinci-Code-Youtube/data/" + targeted_channel + "/" + video_id)
 
     check_downloaded_videos_file(targeted_channel)
-
     update_downloaded_videos_file(targeted_channel, video_id)
+    update_videos_file(targeted_channel, video_id)
 
 
 def check_downloaded_videos_file(targeted_channel):
@@ -46,3 +46,25 @@ def update_downloaded_videos_file(targeted_channel, video_id):
                    video["Video publish time"])
             csv.writer(file_downloaded_videos).writerow(row)
             file_downloaded_videos.close()
+
+
+def update_videos_file(targeted_channel, video_id):
+    videos_list = []
+    file_videos = open(os.path.join("data", targeted_channel, "videos.csv"), "r", newline="")
+    content = csv.DictReader(file_videos)
+    for video in content:
+        videos_list.append(video)
+    file_videos.close()
+
+    file_videos = open(os.path.join("data", targeted_channel, "videos.csv"), "w", newline="")
+    row = ("Channel name", "Video id", "Video title", "Video description", "Video rating", "Video view count",
+           "Video like count", "Video comment count", "Video license", "Video duration", "Video publish time")
+    csv.writer(file_videos).writerow(row)
+    file_videos.close()
+
+    for video in videos_list:
+        if video["Video id"] != video_id:
+            file_videos = open(os.path.join("data", targeted_channel, "videos.csv"), "a", newline="")
+            row = (video["Channel name"], video["Video id"], video["Video title"], video["Video description"], video["Video rating"], video["Video view count"], video["Video like count"], video["Video comment count"], video["Video license"], video["Video duration"], video["Video publish time"])
+            csv.writer(file_videos).writerow(row)
+            file_videos.close()
