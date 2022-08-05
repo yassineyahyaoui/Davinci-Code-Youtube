be8e70cc5231e08a5a06d8c65c3ecc41cd29d2ad
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-08-05 08:41:34 +0100

    update_downloaded_videos_file: file_downloaded_videos

--- functions/upload_videos.py
+++ functions/upload_videos.py
@@ -103,24 +103,24 @@ def update_uploaded_videos_file(targeted_channel, video_id):
 
 def update_downloaded_videos_file(targeted_channel, video_id):
     downloaded_videos_list = []
-    file_videos = open(os.path.join("data", targeted_channel, "downloaded_videos.csv"), "r", newline="")
-    content = csv.DictReader(file_videos)
+    file_downloaded_videos = open(os.path.join("data", targeted_channel, "downloaded_videos.csv"), "r", newline="")
+    content = csv.DictReader(file_downloaded_videos)
     for video in content:
         downloaded_videos_list.append(video)
-    file_videos.close()
+    file_downloaded_videos.close()
 
-    file_videos = open(os.path.join("data", targeted_channel, "downloaded_videos.csv"), "w", newline="")
+    file_downloaded_videos = open(os.path.join("data", targeted_channel, "downloaded_videos.csv"), "w", newline="")
     row = ("Channel name", "Video id", "Video title", "Video description", "Video thumbnail", "Video category", "Video rating", "Video view count",
            "Video like count", "Video comment count", "Video license", "Video duration", "Video publish time")
-    csv.writer(file_videos).writerow(row)
-    file_videos.close()
+    csv.writer(file_downloaded_videos).writerow(row)
+    file_downloaded_videos.close()
 
     for video in downloaded_videos_list:
         if video["Video id"] != video_id:
             file_videos = open(os.path.join("data", targeted_channel, "downloaded_videos.csv"), "a", newline="")
             row = (video["Channel name"], video["Video id"], video["Video title"], video["Video description"], video["Video thumbnail"], video["Video category"], video["Video rating"], video["Video view count"], video["Video like count"], video["Video comment count"], video["Video license"], video["Video duration"], video["Video publish time"])
             csv.writer(file_videos).writerow(row)
-    file_videos.close()
+    file_downloaded_videos.close()
 
 
 def get_authenticated_service(targeted_channel):
