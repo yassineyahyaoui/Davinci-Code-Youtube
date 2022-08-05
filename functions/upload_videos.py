21d7733fa9913e1f596507bae2a182186e26c3a1
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-08-05 08:30:00 +0100

    check_uploaded_videos_file

--- functions/upload_videos.py
+++ functions/upload_videos.py
@@ -66,6 +66,22 @@ def set_thumbnail(targeted_channel, targeted_video_id, video_id):
     print(response)
 
 
+def check_uploaded_videos_file(targeted_channel):
+    if not os.path.exists(os.path.join("data", targeted_channel, "uploaded_videos.csv")):
+        file_uploaded_videos = open(os.path.join("data", targeted_channel, "uploaded_videos.csv"), "w", newline="")
+        file_uploaded_videos.close()
+
+    file_uploaded_videos = open(os.path.join("data", targeted_channel, "uploaded_videos.csv"), "r", newline="")
+    content = file_uploaded_videos.read()
+    file_uploaded_videos.close()
+
+    if ("Channel name" or "Video id" or "Video title" or "Video description" or "Video thumbnail" or "Video rating" or "Video view count" or "Video like count" or "Video comment count" or "Video license" or "Video duration" or "Video publish time") not in content:
+        file_uploaded_videos = open(os.path.join("data", targeted_channel, "uploaded_videos.csv"), "w", newline="")
+        row = ("Channel name", "Video id", "Video title", "Video description", "Video thumbnail", "Video category", "Video rating", "Video view count", "Video like count", "Video comment count", "Video license", "Video duration", "Video publish time")
+        csv.writer(file_uploaded_videos).writerow(row)
+        file_uploaded_videos.close()
+
+
 def update_downloaded_videos_file(targeted_channel, video_id):
     videos_list = []
     file_videos = open(os.path.join("data", targeted_channel, "downloaded_videos.csv"), "r", newline="")
