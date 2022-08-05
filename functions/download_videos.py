89c73f3bcf60188cabf2733c0a694d14bc71d80c
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-08-05 08:03:50 +0100

    download_videos.py video category fixed

--- functions/download_videos.py
+++ functions/download_videos.py
@@ -6,7 +6,15 @@ from pytube import YouTube
 
 
 def download_videos(targeted_channel, video_id):
+    yt = YouTube("https://www.youtube.com/" + video_id)
+    yt.streams.filter(progressive=True, file_extension='mp4').get_highest_resolution().download(os.getcwd() + "/data/" + targeted_channel + "/videos/" + video_id, filename="video" + ".mp4")
+
+    check_downloaded_videos_file(targeted_channel)
+    update_downloaded_videos_file(targeted_channel, video_id)
     sort_downloaded_videos_by_rating(targeted_channel)
+    update_videos_file(targeted_channel, video_id)
+
+    urllib.request.urlretrieve("https://i.ytimg.com/vi/" + video_id + "/hqdefault.jpg", os.getcwd() + "/data/" + targeted_channel + "/videos/" + video_id + "/thumbnail.jpg")
 
 
 def check_downloaded_videos_file(targeted_channel):
@@ -20,7 +28,7 @@ def check_downloaded_videos_file(targeted_channel):
 
     if ("Channel name" or "Video id" or "Video title" or "Video description" or "Video thumbnail" or "Video rating" or "Video view count" or "Video like count" or "Video comment count" or "Video license" or "Video duration" or "Video publish time") not in content:
         file_downloaded_videos = open(os.path.join("data", targeted_channel, "downloaded_videos.csv"), "w", newline="")
-        row = ("Channel name", "Video id", "Video title", "Video description", "Video thumbnail", "Video rating", "Video view count", "Video like count", "Video comment count", "Video license", "Video duration", "Video publish time")
+        row = ("Channel name", "Video id", "Video title", "Video description", "Video thumbnail", "Video category", "Video rating", "Video view count", "Video like count", "Video comment count", "Video license", "Video duration", "Video publish time")
         csv.writer(file_downloaded_videos).writerow(row)
         file_downloaded_videos.close()
 
@@ -37,7 +45,7 @@ def update_downloaded_videos_file(targeted_channel, video_id):
         if video["Video id"] == video_id:
             file_downloaded_videos = open(os.path.join("data", targeted_channel, "downloaded_videos.csv"), "a",
                                           newline="")
-            row = (video["Channel name"], video["Video id"], video["Video title"], video["Video description"], video["Video thumbnail"],
+            row = (video["Channel name"], video["Video id"], video["Video title"], video["Video description"], video["Video thumbnail"], video["Video category"],
                    video["Video rating"], video["Video view count"], video["Video like count"],
                    video["Video comment count"], video["Video license"], video["Video duration"],
                    video["Video publish time"])
@@ -54,7 +62,7 @@ def update_videos_file(targeted_channel, video_id):
     file_videos.close()
 
     file_videos = open(os.path.join("data", targeted_channel, "videos.csv"), "w", newline="")
-    row = ("Channel name", "Video id", "Video title", "Video description", "Video thumbnail", "Video rating", "Video view count",
+    row = ("Channel name", "Video id", "Video title", "Video description", "Video thumbnail", "Video category", "Video rating", "Video view count",
            "Video like count", "Video comment count", "Video license", "Video duration", "Video publish time")
     csv.writer(file_videos).writerow(row)
     file_videos.close()
@@ -62,7 +70,7 @@ def update_videos_file(targeted_channel, video_id):
     for video in videos_list:
         if video["Video id"] != video_id:
             file_videos = open(os.path.join("data", targeted_channel, "videos.csv"), "a", newline="")
-            row = (video["Channel name"], video["Video id"], video["Video title"], video["Video description"], video["Video thumbnail"], video["Video rating"], video["Video view count"], video["Video like count"], video["Video comment count"], video["Video license"], video["Video duration"], video["Video publish time"])
+            row = (video["Channel name"], video["Video id"], video["Video title"], video["Video description"], video["Video thumbnail"], video["Video category"], video["Video rating"], video["Video view count"], video["Video like count"], video["Video comment count"], video["Video license"], video["Video duration"], video["Video publish time"])
             csv.writer(file_videos).writerow(row)
             file_videos.close()
 
