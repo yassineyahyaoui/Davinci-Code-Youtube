7dbc8f038d3e0b1b2a7e85c6210d2cd295f83a1c
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-08-08 01:04:00 +0100

    get only 2 videos per channel

--- functions/get_videos.py
+++ functions/get_videos.py
@@ -16,7 +16,7 @@ def get_videos(targeted_channel, channel_id, shorts):
         part="snippet,id",
         channelId=channel_id,
         order="date",
-        maxResults=5
+        maxResults=2
     )
     response = request.execute()
 
