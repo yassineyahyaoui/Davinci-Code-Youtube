7319b65cc0373d3c5bf93b4b184f2616e60138aa
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2022-08-17 16:55:16 +0100

    new videos

--- main.py
+++ main.py
@@ -8,9 +8,33 @@ import requests
 
 
 def main():
+    get_subscriptions("FOOT BALL")
 
-    download_videos("FOOT BALL", "pvOZ3lOVkaY")
-    upload_videos("FOOT BALL", "pvOZ3lOVkaY")
+    file_channels = open(os.path.join("data", "FOOT BALL", "channels.csv"), "r", newline="")
+    content = csv.DictReader(file_channels)
+    for row in content:
+        get_videos("FOOT BALL", row["Channel id"], True)
+    file_channels.close()
+
+    file_videos = open(os.path.join("data", "FOOT BALL", "videos.csv"), "r", newline="")
+    content = csv.DictReader(file_videos)
+    i = 0
+    for row in content:
+        request = requests.get("https://www.youtube.com/watch?v=" + row["Video id"]).status_code
+        if request == 200:
+            if i < 2:
+                download_videos("FOOT BALL", row["Video id"])
+            i = i + 1
+    file_videos.close()
+
+    file_downloaded_videos = open(os.path.join("data", "FOOT BALL", "downloaded_videos.csv"), "r", newline="")
+    content_downloaded_videos = csv.DictReader(file_downloaded_videos)
+    i = 0
+    for row in content_downloaded_videos:
+        if i < 2:
+            upload_videos("FOOT BALL", row["Video id"])
+        i = i + 1
+    file_downloaded_videos.close()
 
 
 if __name__ == "__main__":
